999b8ab7001f9f5edf206ae3503c7e47cff52973
Ravi Kumar <ravi.kumar2@aristocrat.com>
2026-05-25 15:47:09 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Ravi Kumar",
+    author_email="ravikumarhbti@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
