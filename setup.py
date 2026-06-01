3dd14a7d2fa43adaca4a91a1a3b8fc03f5c5481a
Ravi Kumar <ravi.kumar2@aristocrat.com>
2026-06-01 14:23:30 +0530

    dev complete

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="medical-chatbot",
-    version="0.1.0",
+    version="1.0.0",
     author="Ravi Kumar",
     author_email="ravikumarhbti@gmail.com",
     packages=find_packages(),
